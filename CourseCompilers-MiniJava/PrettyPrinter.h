6242deea7a98a9a46849c645ae439825e482f5f8
ggoha <ryznikov@mail.ru>
2015-10-20 13:15:30 +0400

    Промежуточный

--- CourseCompilers-MiniJava/PrettyPrinter.h
+++ CourseCompilers-MiniJava/PrettyPrinter.h
@@ -9,124 +9,31 @@ class CPrettyPrinter : public IVisitor
 {
 public:
 	void visit(CStatementIF* n);
-	{
-		cout << "if ( ";
-		n->c1->accept(this);
-		cout << ") ";
-		n->c2->accept(this);
-		cout << " else ";
-		n->c3->accept(this);
-	};
 
-	void visit(CStatementBRACKETS* n)
-	{
-		cout << "{ ";
-		n->c1->accept(this);
-		cout << " }";
-	};
+	void visit(CStatementBRACKETS* n);
 
-	void visit(CStatementSQUEREASIGNMENT* n)
-	{
-		n->c1->accept(this);
-		cout << "[ ";
-		n->c2->accept(this);
-		cout << " ] = ";
-		n->c3->accept(this);
-		cout << ";";
-	};
+	void visit(CStatementSQUEREASIGNMENT* n);
 
-	void visit(CStatementASIGNMENT* n)
-	{
-		n->c1->accept(this);
-		cout << " = ";
-		n->c2->accept(this);
-		cout << ";";
-	};
+	void visit(CStatementASIGNMENT* n);
 
-	void visit(CStatementPRINTLN* n)
-	{
-		cout << "( ";
-		n->c1->accept(this);
-		cout << ")";
-	};
+	void visit(CStatementPRINTLN* n);
 
-	void visit(CStatementWHILE* n)
-	{
-		cout << "while ( ";
-		n->c1->accept(this);
-		cout << "0 ";
-		n->c2->accept(this);
-	};
-
+	void visit(CStatementWHILE* n);
 
 	template< class T>
-	void visit(TStar<T>* n)
-	{
-		cout << " ";
-		for (auto i in n->v)
-		{
-			i->accept(this);
-			cout << " ";
-		};
-	}
-	void visit(CExpBinary* n)
-	{
-		n->c1->accept(this);
-		cout << n->c;
-		n->c2->accept(this);
-	}
-	void visit( CExpInSquareBrackets *n ) {
-		n->c1->accept( this );
-		cout << '[';
-		n->c2->accept( this );
-		cout << ']';
-	}
-	void visit( CExpPointLENGTH *n ) {
-		n->c1->accept( this );
-		cout << '.';
-		n->c2->accept( this );
-	}
-	void visit( CExpPointID *n ) {
-		n->c1->accept( this );
-		cout << '.';
-		n->c2->accept( this );
-		cout << '(';
-		n->c3->accept( this );
-		cout << ')';
-	}
-	void visit( CExpINTEGER_LITERAL *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpSingleOp *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpID *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpTHIS *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpNEWINT *n ) {
-		n->c1->accept( this );
-		n->c2->accept( this );
-		cout << '[';
-		n->c3->accept( this );
-		cout << ']';
-	}
-	void visit( CExpNEWID *n ) {
-		n->c1->accept( this );
-		n->c2->accept( this );
-		cout << '(' <<  ')';
-	}
-	void visit( CExpExclamationMark *n ) {
-		cout << '!';
-	}
-	void visit( CExpCircleBrackets *n ) {
-		cout << '(';
-		n->c1->accept( this );
-		cout << ')';
-	}
-
+	void visit(TStar<T>* n);
+	void visit(CExpBinary* n);
+	void visit(CExpInSquareBrackets *n);
+	void visit(CExpPointLENGTH *n);
+	void visit(CExpPointID *n);
+	void visit(CExpINTEGER_LITERAL *n);
+	void visit(CExpSingleOp *n);
+	void visit(CExpID *n);
+	void visit(CExpTHIS *n);
+	void visit(CExpNEWINT *n);
+	void visit(CExpNEWID *n);
+	void visit(CExpExclamationMark *n);
+	void visit(CExpCircleBrackets *n);
 };
 
 
