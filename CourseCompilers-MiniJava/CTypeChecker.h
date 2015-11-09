0772fae5647d4ccfeeab9ee5d8733fb8879269ce
Nikolai <nvvaulin@gmail.com>
2015-11-10 01:06:59 +0300

    typeChecker

--- CourseCompilers-MiniJava/CTypeChecker.h
+++ CourseCompilers-MiniJava/CTypeChecker.h
@@ -81,4 +81,131 @@ public:
 		return type;
 	}
 
+
+	void visit(CExpBinary* n)
+	{
+		n->exp1->accept(this);
+		string typeExp1 = lastTypeValue;
+		n->exp2->accept(this);
+		if (typeExp1 != lastTypeValue)
+			cout << "exception in "  << n->op;
+		if (n->op == '&')
+		{
+			if (lastTypeValue != "boolean")
+				cout << "exception in "  << n->op << " arguments is not boolean" << '\n';
+			lastTypeValue = "boolean";
+		}
+		else
+		{
+			if (lastTypeValue != "boolean")
+				cout << "exception in "  << n->op  << " arguments is not int" << '\n';
+			if (n->op == '<')
+				lastTypeValue = "boolean";
+			else
+				lastTypeValue = "int";
+		};
+	}
+
+	void visit(CExpInSquareBrackets *n)
+	{
+		n->exp1->accept(this);
+		if (lastTypeValue[lastTypeValue.length() - 1] != ']' || lastTypeValue[lastTypeValue.length() - 2] != '[')
+			cout << "exception in array assigment:" << lastTypeValue << " is not array" << '\n';
+		return ;
+		string t = lastTypeValue.substr(0, lastTypeValue.length() - 2);
+		n->exp2->accept(this);
+		if (t != lastTypeValue)
+			cout << "exception in array assigment: " << t << "=" << lastTypeValue << '\n';
+	}
+
+	void visit(CExpPointLENGTH *n)
+	{
+		n->exp->accept(this);
+		if (lastTypeValue != "String")
+			cout << "esception length is not attribute of " << lastTypeValue<<'\n';
+		lastTypeValue = "int";
+	}
+	void visit(CExpPointID *n)
+	{
+		n->exp->accept(this);
+		n->expList->accept(this);
+
+		for (int i = 0; i < table.classInfo[this->classPos].methods.size(); i++)
+			if (table.classInfo[this->classPos].methods[i].name == n->id) {
+				lastTypeValue = table.classInfo[this->classPos].methods[i].returnType;
+			}
+	}
+
+	void visit(CExpINTEGER_LITERAL *n)
+	{
+		lastTypeValue = "int";
+	}
+
+	void visit(CExpSingleOp *n)
+	{
+		lastTypeValue = "boolean";
+	}
+	void visit(CExpID *n)
+	{
+		string t = "";
+		if (assignType(n->id, t))
+			lastTypeValue = t;
+		else
+			cout << n->id << "doesn't exist\n";
+	}
+	void visit(CExpTHIS *n)
+	{
+	}
+	void visit(CExpNEWINT *n)
+	{
+		n->exp->accept(this);
+		if (lastTypeValue != "int")
+			cout << "array index must be int\n";
+		lastTypeValue = "int[]";
+	}
+	void visit(CExpNEWID *n)
+	{
+		if ((n->id != "int") && (n->id != "boolean"))
+			if (!checkClassExistence(n->id))
+				cout << "No such type: " << n->id << endl;
+	}
+	void visit(CExpExclamationMark *n)
+	{
+		n->exp->accept(this);
+		if (lastTypeValue != "boolean")
+			cout << "! just for boolean but called with type " << lastTypeValue << '\n';
+		lastTypeValue = "boolean";
+	}
+	void visit(CExpCircleBrackets *n)
+	{
+		n->exp->accept(this);
+	}
+
+	void visit(CExpUnaryMinus *n)
+	{
+		n->exp->accept(this);
+		if (lastTypeValue != "int")
+			cout << "unary minus must be used with int but called with " << lastTypeValue;
+		lastTypeValue = "int";
+	}
+
+
+	void visit(CType *n)
+	{
+		if (n->inputType == n->_int)
+			lastTypeValue = "int";
+
+		if (n->inputType == n->_bool)
+			lastTypeValue = "boolean";
+
+		if (n->inputType == n->_mas)
+			lastTypeValue = "int[]";
+
+		if (n->inputType == n->_id)
+		{
+			lastTypeValue = n->id;
+			if (!checkClassExistence(lastTypeValue))
+				cout << "class " << n->id << " doesn't exist in the current context\n";
+		};
+	}
 };
