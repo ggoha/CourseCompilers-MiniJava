940c427c17c01b38bc60b0200913bb10440c944a
ggoha <ryznikov@mail.ru>
2015-12-08 15:42:35 +0400

    IRVistor

--- CourseCompilers-MiniJava/IRVisitor.h
+++ CourseCompilers-MiniJava/IRVisitor.h
@@ -9,5 +9,13 @@ public:
 	virtual void visit(IRExpBINOP* n) const {};
 	virtual void visit(IRExpNAME* n) const {};
 	virtual void visit(IRExpTEMP* n) const {};
-	virtual void visit(IRExpCONST *n) {};
+	virtual void visit(IRExpCONST *n) const {};
+
+	virtual void visit(IRStmMOVE *n) const {};
+	virtual void visit(IRStmEXP *n) const {};
+	virtual void visit(IRStmCJUMP *n) const {};
+	virtual void visit(IRStmSEQ *n) const {};
+	virtual void visit(IRStmLABEL *n) const {};
+	virtual void visit(IRStmLIST *n) const {};
+	virtual void visit(IRStmJUMP *n) const {};
 };
